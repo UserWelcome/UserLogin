a0e1e8c0ed996800de5ea238f9f1e98faad6e512
UserWelcome <135332037+UserWelcome@users.noreply.github.com>
2024-08-12 13:28:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,19 +13,19 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(cors());
 // app.use(cors(
 //     {
-//         origin: ["https://userlogin-b8yq.onrender.com","localhost:4000"],
+//         origin: ["https://userlogin-b8yq.onrender.com"],
 //         methods: ["POST", "GET"],
 //         credentials: true
 //     }
 // ));
- app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+ // app.use(function (req, res, next) {
+ //    //Enabling CORS
+ //    res.header("Access-Control-Allow-Origin", "*");
+ //    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+ //    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+ //    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+ //      next();
+ //    });
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/user_routes'));
