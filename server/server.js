d33d022bc238ec1120e6685d83b0ce7b166e8211
UserWelcome <135332037+UserWelcome@users.noreply.github.com>
2024-08-12 16:28:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(cors());
  //      next();
  //    });
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
